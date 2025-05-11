55dd5e789a6b6a361f63b4f8a5647f187f0a0c6f
Krasimir Dramaliev <krasipeace@abv.bg>
2025-05-11 11:57:33 +0300

    Update ChildLabor.cs
    fixed: minimal age to be educated at schools

--- ForcedChildLabor/ChildLabor.cs
+++ ForcedChildLabor/ChildLabor.cs
@@ -13,13 +13,23 @@ namespace ForcedChildLabor
             LoggerInstance.Msg("Forced children labor activated");
         }
 
-        [HarmonyPatch(typeof(VillagerHealth), "Load")]
+        [HarmonyPatch(typeof(VillagerHealth), "Awake")]
         public class PatchVillagerLabor
         {
             protected static void Postfix(VillagerHealth __instance)
             {
                 Traverse.Create(__instance).Field("ageCutoffChild").SetValue(7);
                 Traverse.Create(__instance).Field("ageCutoffAdolescent").SetValue(15);
+            }
+        }
+
+        [HarmonyPatch(typeof(School), "Awake")]
+        public class PatchSchoolForLabor
+        {
+            protected static void Postfix(School __instance)
+            {
+                Traverse.Create(__instance).Field("minEnrollmentAge").SetValue(5);
+                Traverse.Create(__instance).Field("maxEnrollmentAge").SetValue(50);
             }
         }
     }
