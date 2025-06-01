c4d09d052dfd8340b76ef45c33f060a311ac3c12
Krasimir Dramaliev <krasipeace@abv.bg>
2025-06-01 14:13:48 +0300

    added: config support

--- RichVillagers/RichVillager.cs
+++ RichVillagers/RichVillager.cs
@@ -7,15 +7,23 @@ using System.Reflection;
 using System.Linq;
 using System.Data;
 
-[assembly: MelonInfo(typeof(RichVillagers.RichVillager), "Rich Villagers", "1.0.1", "Krasipeace")]
+[assembly: MelonInfo(typeof(RichVillagers.RichVillager), "Rich Villagers", "1.0.2", "Krasipeace")]
 [assembly: MelonGame("Crate Entertainment", "Farthest Frontier")]
 namespace RichVillagers
 {
     public class RichVillager : MelonMod
     {
+        private static MelonPreferences_Category richVillagerConfig;
+        private static MelonPreferences_Entry<int> goldMultiplier;
+        public static int GoldMultiplier => goldMultiplier?.Value ?? 10;
+
         public override void OnInitializeMelon()
         {
-            LoggerInstance.Msg("Villagers are 10 times reacher than normal");
+            richVillagerConfig = MelonPreferences.CreateCategory("RichVillagerConfig", "Rich Villager Config");
+            goldMultiplier = richVillagerConfig.CreateEntry<int>("GoldMultiplier", 10, "Gold Multiplier", "Multiplier for market gold income");
+            MelonPreferences.Save();
+
+            LoggerInstance.Msg($"Gold Income Market Multiplier: x{GoldMultiplier}. Check \\..\\..\\UserData\\MelonPreferences\\[RichVillagerConfig] for a change.");
         }
 
         [HarmonyPatch(typeof(MarketBuilding), "GenerateMonthlyTaxes")]
@@ -25,12 +33,13 @@ namespace RichVillagers
             {
                 var codes = new List<CodeInstruction>(instructions);
                 var fieldInfo = AccessTools.Field(typeof(ExpenseManager.Gain), nameof(ExpenseManager.Gain.amount));
+                var goldMultiplierGetter = AccessTools.PropertyGetter(typeof(RichVillager), nameof(GoldMultiplier));
 
                 for (int i = 0; i < codes.Count - 1; i++)
                 {
                     if (codes[i].opcode == OpCodes.Stfld && codes[i].operand as FieldInfo == fieldInfo)
                     {
-                        codes.Insert(i, new CodeInstruction(OpCodes.Ldc_I4, 10));
+                        codes.Insert(i, new CodeInstruction(OpCodes.Call, goldMultiplierGetter));
                         codes.Insert(i + 1, new CodeInstruction(OpCodes.Mul));
                         i += 2;
                     }
