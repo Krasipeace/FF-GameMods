99abde3b40ff293d2154d7b8e5f68e6349b2ccb3
Krasimir Dramaliev <krasipeace@abv.bg>
2025-10-29 16:06:26 +0200

    Add configurable settings for villager and wagon speeds
    Introduced FastVillagersConfig to allow customization of villager speed, wagon movement speed, and wagon carry capacity via MelonPreferences. Updated FastVillager mod to use these configurable values instead of hardcoded ones.

--- FastVillagers/FastVillager.cs
+++ FastVillagers/FastVillager.cs
@@ -1,19 +1,26 @@
 ﻿using HarmonyLib;
-
 using MelonLoader;
 
-[assembly: MelonInfo(typeof(FastVillagers.FastVillager), "Fast Villagers", "1.0.3", "Krasipeace")]
+[assembly: MelonInfo(typeof(FastVillagers.FastVillager), "Fast Villagers", "1.0.4", "Krasipeace")]
 [assembly: MelonGame("Crate Entertainment", "Farthest Frontier")]
+
 namespace FastVillagers
 {
     public class FastVillager : MelonMod
     {
+        public override void OnInitializeMelon()
+        {
+            FastVillagersConfig.Load();
+            LoggerInstance.Msg("Fast Villagers activated - config file @<YourGameFolder>\\UserData\\MelonPreferences.cfg -> [FastVillagersConfig]");
+        }
+
         [HarmonyPatch(typeof(Character), "Awake")]
         public class PatchCharacterStats
         {
             protected static void Postfix(Character __instance)
             {
-                Traverse.Create(__instance).Field("_shoeBonusBase").SetValue(1f);
+                Traverse.Create(__instance).Field("_shoeBonusBase").SetValue(FastVillagersConfig.VillagerSpeed);
+                Traverse.Create(__instance).Field("_turningSpeed").SetValue(FastVillagersConfig.VillagerSpeed * 50f);
             }
         }
 
@@ -22,9 +29,9 @@ namespace FastVillagers
         {
             protected static void Postfix(TransportWagon __instance)
             {
-                Traverse.Create(__instance).Field("_movementSpeed").SetValue(8f);
-                Traverse.Create(__instance).Field("_turningSpeed").SetValue(100f);
-                __instance.carryCapacity = 400f;
+                Traverse.Create(__instance).Field("_movementSpeed").SetValue(FastVillagersConfig.WagonMoveSpeed);
+                Traverse.Create(__instance).Field("_turningSpeed").SetValue(FastVillagersConfig.WagonMoveSpeed * 50f);
+                __instance.carryCapacity = FastVillagersConfig.WagonCarryCapacity;
             }
         }
     }
---
+++ FastVillagers/FastVillagersConfig.cs
@@ -0,0 +1,27 @@
+﻿using MelonLoader;
+
+namespace FastVillagers
+{
+    public static class FastVillagersConfig
+    {
+        public static float VillagerSpeed => villagerSpeed?.Value ?? 1.0f;
+        public static float WagonMoveSpeed => wagonMoveSpeed?.Value ?? 8.0f;
+        public static float WagonCarryCapacity => wagonCarryCapacity?.Value ?? 400.0f;
+        private static MelonPreferences_Category fvCategory;
+        private static MelonPreferences_Entry<float> villagerSpeed;
+        private static MelonPreferences_Entry<float> wagonMoveSpeed;
+        private static MelonPreferences_Entry<float> wagonCarryCapacity;
+
+        public static void Load()
+        {
+            fvCategory = MelonPreferences.CreateCategory("FastVillagersConfig");
+            villagerSpeed = fvCategory.CreateEntry("VillagerSpeed", 1.0f, "Villager Speed Multiplier", "In case something weird happen, default mod value is: 1.0");
+            wagonMoveSpeed = fvCategory.CreateEntry("WagonMoveSpeed", 8.0f, "Transport Wagon Movement Speed", "In case something weird happen, default mod value is: 8.0");
+            wagonCarryCapacity = fvCategory.CreateEntry("WagonCarryCapacity", 400.0f, "Transport Wagon Carry Capacity", "In case something weird happen, default mod value is: 400.0");
+            MelonPreferences.Save();
+            MelonLogger.Msg($"[FastVillagersConfig] VillagerSpeed = {villagerSpeed.Value}");
+            MelonLogger.Msg($"[FastVillagersConfig] WagonMoveSpeed = {wagonMoveSpeed.Value}");
+            MelonLogger.Msg($"[FastVillagersConfig] WagonCarryCapacity = {wagonCarryCapacity.Value}");
+        }
+    }
+}
